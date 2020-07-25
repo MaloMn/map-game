fcbeaf5c75398862c5b1bcd6f2e3d4485f567d81
Malo Mn <malo.maisonneuve@gmail.com>
2020-07-25 17:40:37 +0200

    Added clicking functionality on Area of country

--- Objects/Area2D.gd
+++ Objects/Area2D.gd
@@ -1,12 +1,12 @@
 extends Area2D
 
-
 export (Color, RGB) var mouse_out
 export (Color, RGB) var mouse_over
 
 var color = PoolColorArray([Color(1.0, 1.0, 1.0)])
 var poly_pts = []
 var collision = CollisionPolygon2D.new()
+var _on_country = false
 
 
 func _draw():
@@ -26,9 +26,16 @@ func init(pts_draw, pts_coll):
 		collision.set_polygon(pts_coll[i])
 
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and _on_country:
+		get_parent().check_answer(self.name)
+
+
 func _on_Area2D_mouse_entered() -> void:
 	set_modulate(mouse_over)
+	_on_country = true
 
 
 func _on_Area2D_mouse_exited() -> void:
 	set_modulate(mouse_out)
+	_on_country = false
