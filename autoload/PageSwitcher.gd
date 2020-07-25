cf2656d9f077569d4b04660e15bfa8ecbfbebe83
Malo Mn <malo.maisonneuve@gmail.com>
2020-07-25 17:39:35 +0200

    Simplified code choosing Pages to display

--- autoload/PageSwitcher.gd
+++ autoload/PageSwitcher.gd
@@ -5,47 +5,49 @@ onready var root = get_tree().get_root()
 onready var current_scene = root.get_child(root.get_child_count() -1)
 
 
-var pages: Array = ["res://Pages/CountryCapital.tscn"] 
-#					"res://Pages/CountryLocation.tscn",
-#					"res://Pages/CapitalLocation.tscn",
-#					"res://Pages/CountryFlag.tscn"]
-var possibilities = [[0, 2]]#, [0, 7], [2, 8], [0, 5]]
+var pages: Array = ["res://Pages/CountryCapital.tscn",
+					"res://Pages/CountryLocation.tscn",
+					"res://Pages/CountryFlag.tscn"]
+#					"res://Pages/CapitalLocation.tscn"]
+var possibilities = [[0, 2], [0, 7], [0, 5]]#, [2, 8]]
 
 var start_scene = "res://Screens/StartScreen.tscn"
 
 
 func _ready():
 	goto_scene(start_scene, null)
+#	goto_scene('res://Pages/CountryLocation.tscn', 'Australia')
 
 
 func start_game(choices):
 	next_level()
+#	goto_scene('res://Pages/CountryLocation.tscn', null)
 
 
 func next_level():
 	# Pick a line from data (not the header)
 	var pages_data = []
-	var pages_binary = []
+	var pages_possible = []
 	
-	while len(pages_binary) == 0 or !pages_binary.has(1):
+	while len(pages_possible) == 0:
+		# Choosing a line from data array
 		var line = DataLoader.data[random_int(1, len(DataLoader.data) - 1)]
 		print("Following line was chosen: ", line)
 		
 		pages_data = []
-		pages_binary = []
-		
-		# For each page, check if we have the data
+		pages_possible = []
+
 		for i in range(len(pages)):
 			var a = possibilities[i][0]
 			var b = possibilities[i][1]
-			if not line[a] or not line[b]:
-				pages_binary.append(0)
-			else:
+			if line[a] and line[b]:
+				# If we have some data corresponding to the page
 				pages_data.append([line[a], line[b]])
-				pages_binary.append(1)
-	
-	var index = random_scene_index(pages_binary)
-	goto_scene(pages[index], pages_data[index])
+				pages_possible.append(pages[i])
+		
+		if len(pages_possible) > 0:
+			var index = random_int(0, len(pages_possible) - 1)
+			goto_scene(pages_possible[index], pages_data[index])
 
 
 func goto_scene(path, param):
@@ -65,18 +67,6 @@ func _deferred_goto_scene(path, param):
 	root.add_child(current_scene)
 	# Optional, to make it compatible with the SceneTree.change_scene() API.
 	get_tree().set_current_scene(current_scene)
-
-
-func random_scene_index(binary):
-	if (len(binary) <= 0) or !binary.has(1):
-		print('Error, got this binary array: ', binary)
-	
-	var array = []
-	for i in range(len(binary)):
-		if binary[i] == 1:
-			array.append(i)
-	
-	return array[random_int(0, len(array) - 1)]
 
 
 func random_int(a: int, b: int):
