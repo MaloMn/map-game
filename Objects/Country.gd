bc462038e00f94486d64450f0ad78d146aac3b08
Malo Mn <malo.maisonneuve@gmail.com>
2020-08-18 16:29:09 +0200

    Renamed Area2D to Country

--- Objects/Country.gd
+++ Objects/Country.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
-export (Color, RGB) var mouse_out
-export (Color, RGB) var mouse_over
+var _mouse_out = Colors.COUNTRY
+var _mouse_over = Colors.COUNTRY_MOUSE_OVER
 
 var color = PoolColorArray([Color(1.0, 1.0, 1.0)])
 var poly_pts = []
@@ -9,15 +9,21 @@ var collision = CollisionPolygon2D.new()
 var _on_country = false
 
 
+func _ready():
+	VisualServer.set_default_clear_color(Colors.BG)
+
+
 func _draw():
 	for p in poly_pts:
 		draw_polygon(p, color)
+#	draw_circle(Vector2(285.6686719, 100.4266648), 10, _mouse_over)
+#	draw_circle(Vector2(360,180), 10, _mouse_over)
 
 
 func init(pts_draw, pts_coll):
 	self.connect("mouse_entered", self, "_on_Area2D_mouse_entered")
 	self.connect("mouse_exited", self, "_on_Area2D_mouse_exited")
-	set_modulate(mouse_out)
+	set_modulate(_mouse_out)
 	poly_pts = pts_draw
 	for i in range(len(pts_coll)):
 		var collision = CollisionPolygon2D.new()
@@ -27,15 +33,17 @@ func init(pts_draw, pts_coll):
 
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.pressed and _on_country:
+	if Input.is_action_pressed("left_click") and _on_country and !get_parent()._clicked:
 		get_parent().check_answer(self.name)
 
 
 func _on_Area2D_mouse_entered() -> void:
-	set_modulate(mouse_over)
-	_on_country = true
+	if !get_parent()._clicked:
+		set_modulate(_mouse_over)
+		_on_country = true
 
 
 func _on_Area2D_mouse_exited() -> void:
-	set_modulate(mouse_out)
-	_on_country = false
+	if !get_parent()._clicked:
+		set_modulate(_mouse_out)
+		_on_country = false
