a4e6b3b4b212b30986fa388d391c9027cf7112f2
Malo Mn <malo.maisonneuve@gmail.com>
2020-09-26 21:26:41 +0200

    First draft of the map display script

--- Objects/world_map_bg.gd
+++ Objects/world_map_bg.gd
@@ -1,14 +1,8 @@
 extends Node2D
 
-var obj_area = preload("res://Objects/Country.tscn")
 var polygons = []
-var color = PoolColorArray([Color(1.0, 1.0, 1.0)])
+var color = PoolColorArray([Colors.COUNTRY])
 
-var question = ""
-var _shortname = ""
-var _clicked = false
-
-var timeout = 2.0  # in seconds
 
 func _draw() -> void:
 	for poly_pts in polygons:
@@ -19,15 +13,9 @@ func _draw() -> void:
 func _ready() -> void:
 	var poly = []
 	for country in DataLoader.polygons.keys():
-#		print(country)
 		poly = DataLoader.polygons[country]
 		if len(poly) >= 2:
 			polygons.append(_country(poly[0]))
-			
-#			var a = obj_area.instance()
-#			a.init(_country(poly[0]), _country(poly[1]))
-#			a.set_name(country)
-#			add_child(a)
 
 
 func _country(poly):
@@ -48,17 +36,7 @@ func init(shortname):
 	pass
 
 
-func _input(event: InputEvent) -> void:
-	if Input.is_action_pressed("ui_accept") and _clicked:
-		PageSwitcher.next_level()
-
-
-func check_answer(a_shortname):
-	_clicked = true
-
-	# Coloring the answer and/or the solution
-	get_node(_shortname).set_modulate(Colors.COUNTRY_GOOD)
-	if a_shortname != _shortname:
-		get_node(a_shortname).set_modulate(Colors.COUNTRY_BAD)
-
-	$"Camera2D".animate_answer(get_node(_shortname).bounding_box(), timeout)
+func color_country(_country, _color):
+	polygons = [DataLoader.polygons[_country][0]]
+	color = PoolColorArray([_color])
+	update()
